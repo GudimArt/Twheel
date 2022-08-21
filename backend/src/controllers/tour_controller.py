ad989fab158f51b6c4e4d93d9d112b705b2c76ac
GuD1mon <89131859292fhntv14@mail.ru>
2022-08-21 22:38:25 +0700

    del finaly

--- backend/src/controllers/tour_controller.py
+++ backend/src/controllers/tour_controller.py
@@ -1,6 +1,6 @@
 from schemas.schema_tour import TourBase
 
-from services.crud.tour_crud import CrudTour
+from services.crud.tour_services import TourService
 
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
@@ -13,20 +13,20 @@ router_tour_controller = InferringRouter()
 class TourController():
     @router_tour_controller.get("/tours")
     async def get_all(self):
-        return CrudTour.get_all(self)
+        return await TourService.get_all(self)
 
     @router_tour_controller.get("/tour/{id}")
     async def get_by_id(self, id:int):
-        return CrudTour.get_by_id(self, id)   
+        return await TourService.get_by_id(self, id)   
 
     @router_tour_controller.post("/tour")
     async def add(self, tour:TourBase):
-        CrudTour.add(self, tour)
+        await TourService.add(self, tour)
 
     @router_tour_controller.delete("/tours/{id}")
     async def delete(self, id:int):
-        CrudTour.delete(self, id)
+        await TourService.delete(self, id)
     
     @router_tour_controller.put("/tour")
     async def update(self, tour:TourBase):
-        CrudTour.update(self, tour)
+        await TourService.update(self, tour)
