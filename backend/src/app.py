2c1f62cf524ff58534a4db07e9393fb18bc9226f
GuD1mon <89131859292fhntv14@mail.ru>
2022-08-21 18:48:23 +0700

    divided the logic

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,7 +4,7 @@ from models.place import Place
 from models.route import Route
 from models.tour import Tour 
 
-from services.crud.crud_tour import router_crud_tour
+from controllers.tour_controller import router_tour_controller
 
 from fastapi import FastAPI
 from fastapi import APIRouter
@@ -14,7 +14,7 @@ router = APIRouter()
 
 Base.metadata.create_all()
 
-app.include_router(router_crud_tour, tags=["tour"])
+app.include_router(router_tour_controller, tags=["tour_controller"])
 
 @app.get("/")
 async def root():
